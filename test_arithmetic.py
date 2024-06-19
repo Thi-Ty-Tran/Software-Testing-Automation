468d0c1e9561178ca5800f9f791e447f8bc8df76
Thi-Ty-Tran <ty.tran@dcmail.ca>
2024-06-18 21:14:00 -0400

    new commit

--- test_arithmetic.py
+++ test_arithmetic.py
@@ -1,20 +1,25 @@
-# test_arithmetic.py initial file
+"""Test_arithmetic.py initial file"""
 
 import pytest
 from arithmetic import add, subtract, multiply, divide
 
 def test_add():
+    """Test add function"""
     assert add(1, 2) == 3
 
 def test_subtract():
+    """Test substraction function"""
     assert subtract(2, 1) == 1
 
 def test_multiply():
+    """ Test multiply function"""
     assert multiply(2, 3) == 6
 
 def test_divide():
+    """Test divide function"""
     assert divide(6, 2) == 3
 
 def test_divide_by_zero():
+    """Test divide by zero function"""
     with pytest.raises(ValueError):
         divide(1, 0)
